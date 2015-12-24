f590f07dc95eaeae41d1bc9f4f657c9698d4fda6
JamieXu <jueduizone@gmail.com>
2015-12-24 11:05:54 +0800

    Memcache is completed,memcache and memcached is all supported

--- cache/MemcacheOperate.php
+++ cache/MemcacheOperate.php
@@ -8,9 +8,37 @@
  */
 class MemcacheOperate
 {
-    private $_host = '127.0.0.1';
-    private $_port = '11211';
+    private $_instance;
+    private $type = 'Memcached';
+    private $serverArr = array(
+        array('127.0.0.1', 11211)
+    );
 
+    /**
+     * MemcacheOperate constructor.
+     * 根据参数实例化 Memcache 或者 Memcached,然后进行连接
+     * Memcached时 如果传入数组数据,没有手动序列化时(json_encode),会自动使用igbinary拓展进行序列化传入,因此需要
+     * 安装igbinary拓展,否则会报错
+     *
+     */
+    public function __construct()
+    {
+        if (!class_exists($this->type)) {
+            return false;
+        } else {
+            $this->_instance = new $this->type;
+            if ($this->type === 'Memcached') {
+                $this->_instance->addServers($this->serverArr);
+            } else {
+                foreach ($this->serverArr as $value) {
+                    $this->_instance->addServer($value[0], $value[1]);
+                }
+            }
+
+            return $this->_instance;
+
+        }
+    }
     /**
      * 读写缓存操作,注意 Memcache, Memcached 两种拓展方式的不同
      *
@@ -20,32 +48,15 @@ class MemcacheOperate
      */
     public function cacheData($key, $value = '', $cacheTime = 0)
     {
-        //Memcache 拓展使用
-        /**
-         * $memcache = new Memcache();
-         * $memcache->connect($this->_host, $this->_port);
-         * echo "Memcahe start\n";
-         * $memcache->set('key',"hello world => Memcahe",MEMCACHE_COMPRESSED,30);
-         * echo $memcache->get('key');
-         * echo "\nMemcahe end\n";
-         **/
-
-        // Memcached 拓展使用
-        $memcached = new Memcached();
-        $memcached->addServer($this->_host, $this->_port);
-
         //没有值时缓存数据
         if ($value !== '') {
             //值 null 时表示删除
             if (is_null($value)) {
-                return $memcached->delete($key);
+                return $this->_instance->delete($key);
             }
-            return $memcached->set($key, $value, $cacheTime);
+            return $this->_instance->set($key, $value, $cacheTime);
         }
         //取出缓存数据
-        echo $value = $memcached->get($key);
+        return $this->_instance->get($key);
     }
 }
-
-$demo = new MemcacheOperate();
-$demo->cacheData('age');
